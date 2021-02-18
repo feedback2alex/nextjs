aeef09d640259562cb5468fbeaa6c583e0229a33
feedback2alex <79275247+feedback2alex@users.noreply.github.com>
2021-02-18 19:56:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,4 @@
-
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-    
-    
-    <html>
+<html>
 <head>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js"></script>
 </head>
@@ -62,20 +48,3 @@ document.oncontextmenu = document.body.oncontextmenu = function() {return false;
 </body>
 
 </html>
-        
-    
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
